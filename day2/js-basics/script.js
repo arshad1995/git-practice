13dcf0df6fc905978f32541bbefccbff90f1c82f
arshad1995 <arshadarshu445@gmail.com>
2019-04-17 11:04:15 +0530

    ifstatement

--- day2/js-basics/script.js
+++ day2/js-basics/script.js
@@ -4,12 +4,24 @@
 
 console.log(getrandom(2));*/
 
-function getRandomAdvanced(type, val) {
-  if (type == "floor") {
-    return Math.floor(val);
+//function getRandomAdvanced(type, val) {
+// if (type == "floor") {
+//  return Math.floor(val);
+// } else {
+//  return Math.ceil(val);
+// }
+//}
+
+//console.log(getRandomAdvanced("floor", 2.5));
+function getgrades(marks) {
+  if (marks >= 80) {
+    return "Distinction";
+  } else if (marks >= 60) {
+    return "firstclass";
+  } else if (marks >= 35) {
+    return "secondclass";
   } else {
-    return Math.ceil(val);
+    return "fail";
   }
 }
-
-console.log(getRandomAdvanced("floor", 2.5));
+console.log(getgrades(35));
