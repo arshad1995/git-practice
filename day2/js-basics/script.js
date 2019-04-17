27400e3967f55e0ef61a17baf041f38da4bcb956
arshad1995 <arshadarshu445@gmail.com>
2019-04-17 16:52:41 +0530

    addfunction

--- day2/js-basics/script.js
+++ day2/js-basics/script.js
@@ -42,34 +42,39 @@ console.log(getrandom(2));*/
 // var fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.splice(2, 0, "dates", "kiwi");
 // console.log(fruits);
-let arry1 = [0, 1, 2, 3, 4];
-let arry2 = [5, 6, 7, 8, 9];
+// let arry1 = [0, 1, 2, 3, 4];
+// let arry2 = [5, 6, 7, 8, 9];
 
-function concatArray(arry1, arry2, num) {
-  let arry3 = arry1.concat(arry2);
-  console.log(arry3);
-  arry3.push(num);
-  console.log(arry3);
-  arry3.sort();
-  console.log(arry3);
-  arry3.reverse();
-  console.log(arry3);
-  let arry4 = arry3.slice(0, 3);
-  console.log(arry4);
-  console.log(arry3);
-  //   arry3.pop();
-  //   arry3.pop();
-  //arry3.pop();
+// function concatArray(arry1, arry2, num) {
+//   let arry3 = arry1.concat(arry2);
+//   console.log(arry3);
+//   arry3.push(num);
+//   console.log(arry3);
+//   arry3.sort();
+//   console.log(arry3);
+//   arry3.reverse();
+//   console.log(arry3);
+//   let arry4 = arry3.slice(0, 3);
+//   console.log(arry4);
+//   console.log(arry3);
+//   //   arry3.pop();
+//   //   arry3.pop();
+//   //arry3.pop();
 
-  console.log(arry3);
-  arry3.pop();
-  console.log(arry3);
-  //   let stringArray = arry3.toString();
-  //   console.log(stringArray);
-}
-concatArray(arry1, arry2, 5);
+//   console.log(arry3);
+//   arry3.pop();
+//   console.log(arry3);
+//   //   let stringArray = arry3.toString();
+//   //   console.log(stringArray);
+// }
+// concatArray(arry1, arry2, 5);
 
 // function add(a, b) {
 //   console.log(a + b);
 // }
 // add(1, 3);
+
+function numbers(ad, cd) {
+  console.log(ad + cd);
+}
+numbers(5, 6);
