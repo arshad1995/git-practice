2a27505dab24e1bdabf8426f4421e96612f00ff9
arshad1995 <arshadarshu445@gmail.com>
2019-04-17 14:52:36 +0530

    array

--- day2/js-basics/script.js
+++ day2/js-basics/script.js
@@ -13,15 +13,39 @@ console.log(getrandom(2));*/
 //}
 
 //console.log(getRandomAdvanced("floor", 2.5));
-function getgrades(marks) {
-  if (marks >= 80) {
-    return "Distinction";
-  } else if (marks >= 60) {
-    return "firstclass";
-  } else if (marks >= 35) {
-    return "secondclass";
-  } else {
-    return "fail";
-  }
+// function getgrades(marks) {
+//   if (marks >= 80) {
+//     return "Distinction";
+//   } else if (marks >= 60) {
+//     return "firstclass";
+//   } else if (marks >= 35) {
+//     return "secondclass";
+//   } else {
+//     return "fail";
+//   }
+// }
+// console.log(getgrades(34));
+
+// function getRandomAdvanced(a, b) {
+//   return a + b;
+// }
+
+// console.log(getRandomAdvanced(3, 2));
+// var arr1 = ["Cecilie", "Lone"];
+// var arr2 = ["Emil", "Tobias", "Linus"];
+// var arr3 = ["Robin", "Morgan"];
+// var myarr = arr1.concat(arr2);
+// console.log(myarr);
+// var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// var citrus = fruits.slice(0, 4);
+// console.log(citrus);
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "dates", "kiwi");
+// console.log(fruits);
+var points = [40, 100, 1, 5, 25, 10];
+function myFunction() {
+  points.sort(function(a, b) {
+    return a - b;
+  });
 }
-console.log(getgrades(35));
+console.log(myFunction());
